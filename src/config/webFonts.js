9dfe6592f6dac3372f59271351fd0ba50bee3ffc
bondarenko199 <bondarenko199@gmail.com>
2018-05-31 22:49:09 +0300

    added google fonts

---
+++ src/config/webFonts.js
@@ -0,0 +1,11 @@
+import WebFont from 'webfontloader'
+
+WebFont.load({
+  google: {
+    families: [
+      'Open+Sans:400,600,700',
+      'Raleway:300,400',
+      'PT+Sans'
+    ]
+  }
+})
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
 
-import './index.css';
+import './config/webFonts'
+import './index.css'
 import 'font-awesome/css/font-awesome.min.css'
 
 const rootNode = document.getElementById('root')
@@ -16,4 +17,4 @@ else {
   document.write('Service is currently under maintenance. Please come back later.')
 }
 
-registerServiceWorker();
+registerServiceWorker()
